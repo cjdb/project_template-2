a7c7ec6ac7af2923d041b9a23878646fbf226337
Christopher Di Bella <cjdb.ns@gmail.com>
2024-01-07 21:23:19 -0800

    adds LIT as a test framework

---
+++ test/lit.cfg.py
@@ -0,0 +1,42 @@
+# Copyright (c) LLVM Foundation
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+import lit.formats
+import lit.util
+import os
+import platform
+import re
+import subprocess
+import sys
+import tempfile
+from pathlib import Path
+
+from lit.llvm import llvm_config
+from lit.llvm.subst import ToolSubst, FindTool
+
+config.name = 'project_template'
+config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
+config.suffixes = ['.test']
+config.excludes = []
+config.test_source_root = os.path.dirname(__file__)
+config.test_exec_root = os.path.join(config.clang_obj_root, 'test')
+homedir = tempfile.TemporaryDirectory()
+config.environment['HOME'] = homedir.name
+
+llvm_config.use_default_substitutions()
+llvm_config.use_clang()
+
+config.substitutions.append(('%PATH%', config.environment['PATH']))
+
+config.substitutions.append((
+    '%new-project',
+    f'{Path(__file__).resolve().parent.resolve().parent}/generate.py 2>&1 new-project %t/project'
+))
+config.substitutions.append(
+    ('%cmake', 'cmake -S%t/project -B%t/project/build -GNinja 2>&1'))
+config.substitutions.append(('%ninja', 'ninja -C%t/project/build 2>&1'))
+config.substitutions.append(
+    ('%ctest', 'ctest --test-dir %t/project/build 2>&1'))
+
+tool_dirs = []
+tools = []
