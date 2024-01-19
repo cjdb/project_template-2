4cdb24d858a45896f11fa767ac9e654edf00e3ce
Christopher Di Bella <cjdb.ns@gmail.com>
2024-01-19 00:35:24 -0800

    changes option to env var

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -22,6 +22,7 @@ config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = os.path.join(config.clang_obj_root, 'test')
 homedir = tempfile.TemporaryDirectory()
 config.environment['HOME'] = homedir.name
+config.environment['VCPKG_MAX_CONCURRENCY'] = '2'
 
 llvm_config.use_default_substitutions()
 llvm_config.use_clang()
@@ -32,10 +33,8 @@ config.substitutions.append((
     '%new-project',
     f'{Path(__file__).resolve().parent.resolve().parent}/generate.py 2>&1 new-project %t/project'
 ))
-config.substitutions.append((
-    '%cmake',
-    'cmake -S%t/project -B%t/project/build -GNinja 2>&1 -DVCPKG_MAX_CONCURRENCY=2'
-))
+config.substitutions.append(
+    ('%cmake', 'cmake -S%t/project -B%t/project/build -GNinja 2>&1'))
 config.substitutions.append(('%ninja', 'ninja -C%t/project/build -v -j1 2>&1'))
 config.substitutions.append(
     ('%ctest', 'ctest --test-dir %t/project/build --output-on-failure 2>&1'))
